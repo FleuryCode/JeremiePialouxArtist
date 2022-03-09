d75d800ad2e399e45fae779ae6533084a88f69c3
Julien Fleury <you@example.comjulienfleury93@gmail.com>
2022-03-09 18:50:09 +0100

    Adding the starting file/folder structure.

---
+++ src/pages/aboutPage/about.page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './about.styles.scss';
+
+const AboutPage = () => {
+    return (
+        <div className="aboutPageContainer">
+
+        </div>
+    );
+}
+
+export default AboutPage;
---
+++ src/pages/homePage/homePage.page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './homePage.styles.scss';
+
+const HomePage = () => {
+    return (
+        <div className="homepageContainer">
+
+        </div>
+    );
+}
+
+export default HomePage;
