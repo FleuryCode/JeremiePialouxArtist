875c15cdbc16897285404949a9f5a8a3605bf505
Julien Fleury <julien@fleuryphotography.com>
2022-04-09 13:06:07 +0200

    Style changes.

--- src/components/portfolioContainer/portfolioContainer.component.jsx
+++ src/components/portfolioContainer/portfolioContainer.component.jsx
@@ -18,16 +18,16 @@ const PortfolioContainer = ({ imagesDownloading, imageData }) => {
             <div className={`${imagesDownloading ? 'd-flex justify-content-center' : 'd-none'} row py-3 px-0 m-0`}>
                 {
                     imageData.map(image => (
-                        <div key={image.id} className="col-12 col-md-4">
+                        <div key={image.id} className="col-12 col-md-6">
                             <ImageLoading />
                         </div>
                     ))
                 }
             </div>
-            <div className={`${imagesDownloading ? 'd-none' : 'd-flex'} row py-3 px-0 m-0`}>
+            <div className={`${imagesDownloading ? 'd-none' : 'd-flex'} row py-3 px-0 m-0 portfolioImagesHolder`}>
                 {
                     imageData.map((image) => (
-                        <div key={image.id} className="col-12 col-md-6 col-lg-4 p-0 m-0">
+                        <div key={image.id} className={`col-12 col-md-5 p-0 m-0`}>
                             <PortfolioImage image={image} />
                         </div>
                     ))
--- src/components/portfolioImage/portfolioImage.component.jsx
+++ src/components/portfolioImage/portfolioImage.component.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import './portfolioImage.styles.scss';
 import { Link } from "react-router-dom";
 
-const PortfolioImage = ({image}) => {
+const PortfolioImage = ({ image }) => {
     return (
-        <Link to={`/portfolio/${image.link}`} className="portfolioImageContainer">
-            <img src={image.src} alt="Portfolio Piece" />
-        </Link>
+        <div className="portfolioImageContainer">
+            <Link to={`/portfolio/${image.link}`} className="portfolioImageLink">
+                <img src={image.src} alt="Portfolio Piece" />
+            </Link>
+        </div>
+
     );
 }
 
