c95162c73d9f9f1bca08df45bf03593afe635f8e
Julien Fleury <julien@fleuryphotography.com>
2022-04-12 17:08:02 +0200

    Removed arrows from changing pages.

--- src/components/portfolioPiece/portfolioPiece.component.jsx
+++ src/components/portfolioPiece/portfolioPiece.component.jsx
@@ -96,16 +96,10 @@ const PortfolioPiece = ({ imageData }) => {
                 <div className="col-12">
                     <div className="portfolioNavigation">
                         <Link to={`/portfolio/${prevLink}`} className="prevContainer me-auto ms-4">
-                            <div className="prevArrow me-3">
-                                <ArrowIcon className="arrowIcon" />
-                            </div>
                             <p>Précédent</p>
                         </Link>
                         <Link to={`/portfolio/${nextLink}`} className="nextContainer ms-auto me-4">
                             <p>Suivant</p>
-                            <div className="nextArrow ms-3">
-                                <ArrowIcon className="arrowIcon" />
-                            </div>
                         </Link>
                     </div>
                 </div>
