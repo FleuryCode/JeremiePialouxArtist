6eff19293305fbc5bc6905a358036f68f0063d76
Julien Fleury <julien@fleuryphotography.com>
2022-04-12 16:00:15 +0200

    Changing styles of inputs, making more minimalist.

--- src/pages/contactPage/contact.page.jsx
+++ src/pages/contactPage/contact.page.jsx
@@ -15,16 +15,12 @@ const ContactPage = () => {
     const [messageSent, setMessageSent] = useState(false);
     const [displayMessage, setDisplayMessage] = useState('');
     // Message Data
-    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
 
     const inputChangeHandle = (event) => {
         const { value, name } = event.target;
         switch (name) {
-            case 'nom':
-                setName(value)
-                break;
             case 'email':
                 setEmail(value)
                 break;
@@ -49,7 +45,6 @@ const ContactPage = () => {
     // Axios Sending
     const sendAxiosMessage = async () => {
         const payload = {
-            nom: name,
             email: email,
             message: message,
             "g-recaptcha-response": recaptchaToken
@@ -59,7 +54,6 @@ const ContactPage = () => {
         try {
             await axios.post(formSparkUrl, payload);
             // Reset Fields
-            setName('');
             setEmail('');
             setMessage('');
             setDisplayMessage('Merci pour votre message');
@@ -76,7 +70,7 @@ const ContactPage = () => {
     };
 
     const sendButtonClick = async (event) => {
-        if(name !== '' && email !== '' && message !== '') {
+        if(email !== '' && message !== '') {
             event.preventDefault();
             setMessageSending(true);
             await sendAxiosMessage();
@@ -93,18 +87,15 @@ const ContactPage = () => {
     return (
         <div className="contactPageContainer container-fluid">
             <div className="row">
-                <div className="col-12 d-flex justify-content-center">
-                    <h1>CONTACTEZ-NOUS</h1>
+                <div className="col-12 d-flex">
+                    <h1>CONTACT</h1>
                 </div>
             </div>
             <form className="row p-4">
-                <div className="col-12">
-                    <CustomInput id={'nom'} type={'text'} name={'nom'} placeholder={'NOM COMPLET'} value={name} onChange={inputChangeHandle} />
-                </div>
-                <div className="col-12">
+                <div className="col-12 my-2 d-flex justify-content-center">
                     <CustomInput id={'email'} type={'email'} name={'email'} placeholder={'EMAIL'} value={email} onChange={inputChangeHandle} />
                 </div>
-                <div className="col-12 mb-3">
+                <div className="col-12 my-4 d-flex justify-content-center">
                     <CustomTextArea id={'message'} name={'message'} placeholder={'MESSAGE'} value={message} onChange={inputChangeHandle} />
                 </div>
                 <div className="col-12 col-md-8 mb-4">
