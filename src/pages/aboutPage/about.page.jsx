7c113ee8425f615295c6683cee36e8e0ad10326a
Julien Fleury <julienfleury93@gmail.com>
2022-04-28 14:32:02 +0200

    Bio is now mobile friendly.

--- src/pages/aboutPage/about.page.jsx
+++ src/pages/aboutPage/about.page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './about.styles.scss';
 import ProfilePicture from '../../assets/lowQualProfile.jpeg';
-import { peinture, abstrait, matiere, lumiere, invitation } from "../../websiteText";
+import { peinture, abstrait, matiere, lumiere } from "../../websiteText";
 // Redux
 import { connect } from "react-redux";
 
@@ -9,7 +9,7 @@ const AboutPage = ({ textData }) => {
     // TEXT CHANGES
     // Peinture
     let peintureText = peinture;
-    
+
     // Abstrait
     let abstraitText = abstrait;
 
@@ -19,12 +19,10 @@ const AboutPage = ({ textData }) => {
     // Lumière et Couleur
     let lumiereText = lumiere;
 
-    // Invitation
-    let invitationText = invitation;
-    
+
     return (
         <div className="aboutPageContainer container-fluid">
-            <div className="row aboutInfo">
+            <div className="row aboutInfo d-none d-md-flex">
                 <div className="col-6 p-4">
                     {/* Peinture */}
                     <div className="peinture">
@@ -48,6 +46,41 @@ const AboutPage = ({ textData }) => {
                     </div>
                     {/* Lumière et Couleur */}
                     <div className="lumiere mt-5">
+                        <h2>Lumière et Couleur</h2>
+                        <p>{lumiereText}</p>
+                    </div>
+                </div>
+            </div>
+            <div className="row aboutInfo d-flex d-md-none">
+                <div className="col-12 p-4">
+                    <div className="profilePicture d-flex justify-content-center">
+                        <img src={ProfilePicture} alt="Kamonn Profile" />
+                    </div>
+                </div>
+                <div className="col-12 p-4">
+                    {/* Peinture */}
+                    <div className="peinture">
+                        <h2>Peinture</h2>
+                        <p>{peintureText}</p>
+                    </div>
+                </div>
+                <div className="col-12 p-4">
+                    {/* Abstrait */}
+                    <div className="abstrait">
+                        <h2>Abstrait</h2>
+                        <p>{abstraitText}</p>
+                    </div>
+                </div>
+                <div className="col-12 p-4">
+                    {/* Matiere */}
+                    <div className="matiere">
+                        <h2>Matière</h2>
+                        <p>{matiereText}</p>
+                    </div>
+                </div>
+                <div className="col-12 p-4">
+                    {/* Lumière et Couleur */}
+                    <div className="lumiere">
                         <h2>Lumière et Couleur</h2>
                         <p>{lumiereText}</p>
                     </div>
